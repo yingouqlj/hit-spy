530be0fafdb931b66552a677d179b2032bb89b75
yingouqlj <yingouqlj@gmail.com>
2017-01-22 01:59:05 +0800

    ad

---
+++ src/Server.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yingouqlj
+ * Date: 17/1/22
+ * Time: 上午1:54
+ */
+
+namespace Yingou\HitSpy;
+
+
+class Server
+{
+
+    public function run()
+    {
+
+    }
+}
